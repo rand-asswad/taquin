bd4c8a2a73f2beda76e6a347b7c561a15578b0a0
Rand Asswad <rand.asswad@insa-rouen.fr>
2019-11-29 02:40:39 +0100

    print different sizes

--- util/print.pl
+++ util/print.pl
@@ -1,4 +1,8 @@
-printState(P) :- format('~d  ~d  ~d\n~d  ~d  ~d\n~d  ~d  ~d\n', P).
+:- dynamic dim/2, solvePath/2.
+
+printState(P) :- dim(3,3), format('~d  ~d  ~d\n~d  ~d  ~d\n~d  ~d  ~d\n', P).
+printState(P) :- dim(3,4), format('~d  ~d  ~d  ~d\n~d  ~d  ~d  ~d\n~d  ~d  ~d  ~d\n', P).
+printState(P) :- dim(4,4), format('~d  ~d  ~d  ~d\n~d  ~d  ~d  ~d\n~d  ~d  ~d  ~d\n~d  ~d  ~d  ~d\n', P).
 
 :- consult(moves).
 
@@ -7,3 +11,9 @@ printSolution(Puzzle, [M|Path]) :-
     printState(Puzzle), % initial state
     writeln(M), move(Puzzle, Next, M), % print move
     printSolution(Next, Path). % print remaining
+
+testPuzzle(Puzzle) :-
+    solvePath(Puzzle, Path),
+    printSolution(Puzzle, Path),
+    length(Path, N),
+    format('# of moves: ~d', N).
