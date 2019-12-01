73f05cfe9c5f3797c03dafdc2d9ebe913cdc40d9
Rand Asswad <rand.asswad@insa-rouen.fr>
2019-12-01 17:11:25 +0100

    Better printing

--- util/print.pl
+++ util/print.pl
@@ -1,16 +1,35 @@
-:- dynamic dim/2, move/3, solve/3.
+/* module for testing and displaying 'taquin' puzzle
+ * uses:
+ * - dim/2
+ * - move/3
+ * - solve/3, solve/4
+ * - setNth0/4
+ * provides:
+ * - printState/1
+ * - printSolution/2
+ * - testPuzzle/3, testPuzzle/2, testPuzzle/1
+ */
 
-printState(P) :- dim(3,3), format('~d  ~d  ~d\n~d  ~d  ~d\n~d  ~d  ~d\n', P).
-printState(P) :- dim(3,4), format('~d  ~d  ~d  ~d\n~d  ~d  ~d  ~d\n~d  ~d  ~d  ~d\n', P).
-printState(P) :- dim(4,4), format('~d  ~d  ~d  ~d\n~d  ~d  ~d  ~d\n~d  ~d  ~d  ~d\n~d  ~d  ~d  ~d\n', P).
+:- dynamic dim/2, move/3, solve/3, solve/4, setNth0/4.
 
-:- consult(moves).
+removeZero(S0, S) :- nth0(Ind, S0, 0), setNth0(Ind, ' ', S0, S).
+printState(S0) :- removeZero(S0, S), printState(S).
+printState(S) :- dim(3,3), format('~a  ~a  ~a\n~a  ~a  ~a\n~a  ~a  ~a\n', S).
+printState(S) :- dim(3,4), format('~a  ~a  ~a  ~a\n~a  ~a  ~a  ~a\n~a  ~a  ~a  ~a\n', S).
+printState(S) :- dim(4,4), format('~a  ~a  ~a  ~a\n~a  ~a  ~a  ~a\n~a  ~a  ~a  ~a\n~a  ~a  ~a  ~a\n', S).
 
 printSolution(State, []) :- printState(State).
 printSolution(Puzzle, [S|Path]) :-
     printState(Puzzle), % initial state
     move(Puzzle, S, M), writeln(M), % print move
     printSolution(S, Path). % print remaining
+
+testPuzzle(Puzzle, Algorithm, H) :-
+    solve(Puzzle, Solution, Algorithm, H),
+    reverse(Solution, Path),
+    printSolution(Puzzle, Path),
+    length(Path, N),
+    format('# of moves: ~d\n', N).
 
 testPuzzle(Puzzle, Algorithm) :-
     solve(Puzzle, Solution, Algorithm),
